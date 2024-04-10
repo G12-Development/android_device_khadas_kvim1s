G12-Development/android_device_amlogic_m98

kvim1s: rebrand from deadpool

extract-files.sh
@@ -14,9 +14,9 @@ fi
 
 set -e
 
-export DEVICE=deadpool
+export DEVICE=kvim1s
 export DEVICE_COMMON=g12-common
 export VENDOR_COMMON=amlogic
-export VENDOR_DEVICE=askey
+export VENDOR_DEVICE=khadas
 
 "./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"

setup-makefiles.sh
@@ -8,9 +8,9 @@
 
 set -e
 
-export DEVICE=deadpool
+export DEVICE=kvim1s
 export DEVICE_COMMON=g12-common
 export VENDOR_COMMON=amlogic
-export VENDOR_DEVICE=askey
+export VENDOR_DEVICE=khadas
 
 "./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"

G12-Development/android_device_khadas_kvim1s

extract-files.sh
@@ -14,9 +14,9 @@ fi
 
 set -e
 
-export DEVICE=deadpool
-export DEVICE_COMMON=g12-common
-export VENDOR=askey
+export DEVICE=kvim1s
+export DEVICE_COMMON=ne-common
+export VENDOR=khadas
 export VENDOR_COMMON=amlogic
 
 "./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"

setup-makefiles.sh
@@ -8,9 +8,9 @@
 
 set -e
 
-export DEVICE=deadpool
-export DEVICE_COMMON=g12-common
-export VENDOR=askey
+export DEVICE=kvim1s
+export DEVICE_COMMON=ne-common
+export VENDOR=khadas
 export VENDOR_COMMON=amlogic
 
 "./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"